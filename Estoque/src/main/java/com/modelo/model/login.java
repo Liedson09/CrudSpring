5f9939ad03f115c492b5fc8a740e9ccf0d9a004b
Liedson <manoelliedson11@gmail.com>
2019-12-12 09:18:04 -0300

    v7

--- Estoque/src/main/java/com/modelo/model/login.java
+++
@@ -1,5 +0,0 @@
-package com.modelo.model;
-
-public class login {
-
-}
--- Estoque/src/main/java/com/modelo/repository/CrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.modelo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.modelo.model.Livro;
-
-public interface CrudRepository extends org.springframework.data.repository.CrudRepository<Livro, Long> {
-
-}
